a79777218911532f0480b0ec2ea9351b68703ace
GrimmFromIG <gamerboy200715@gmail.com>
2025-03-11 18:16:24 +0200

    с

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,74 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Виберіть пункт за допомогою цифри - \n" +
+                          "1. Додати студента \n" +
+                          "2. Змінити дані про студента \n" +
+                          "3. Переглянути список студентів \n" +
+                          "4. Вийти з програми");
+
+        StudentTable table = new StudentTable();
+        bool running = true;
+
+        while (running)
+        {
+            Console.Write("Ваш вибір: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введіть ім'я: ");
+                    string firstName = Console.ReadLine();
+                    Console.Write("Введіть прізвище: ");
+                    string lastName = Console.ReadLine();
+                    Console.Write("Введіть по батькові: ");
+                    string middleName = Console.ReadLine();
+
+                    table.AddStudent(new Student(firstName, lastName, middleName));
+                    Console.WriteLine($"Студента додано! Його індекс: {table.StudentCount - 1}\n");
+                    break;
+
+                case "2":
+                    Console.Write("Введіть індекс студента для редагування: ");
+                    if (int.TryParse(Console.ReadLine(), out int index) && index >= 0 && index < table.StudentCount)
+                    {
+                        Console.Write("Введіть нове ім'я: ");
+                        string newFirstName = Console.ReadLine();
+                        Console.Write("Введіть нове прізвище: ");
+                        string newLastName = Console.ReadLine();
+                        Console.Write("Введіть нове по батькові: ");
+                        string newMiddleName = Console.ReadLine();
+
+                        table.UpdateStudent(index, newFirstName, newLastName, newMiddleName);
+                        Console.WriteLine("Дані студента оновлено!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Помилка: Неправильний індекс!\n");
+                    }
+                    break;
+                
+                case "3":
+                    Console.WriteLine("Список студентів:");
+                    for (int i = 0; i < table.StudentCount; i++)
+                    {
+                        Console.WriteLine($"{i}: {table[i]}");
+                    }
+        
+                    Console.WriteLine($"Кількість студентів: {table.StudentCount}");
+                    break;
+                    
+                case "4":
+                    running = false;
+                    Console.WriteLine("Вихід з програми...");
+                    break;
+
+                default:
+                    Console.WriteLine("Неправильний вибір. Спробуйте ще раз.\n");
+                    break;
+            }
+        }
+    }
+}
---
+++ HomeTask1/StudentTable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+
+    public Student(string firstName, string lastName, string middleName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        MiddleName = middleName;
+    }
+    
+    public override string ToString()
+    {
+        return $"{FirstName} {MiddleName} {LastName}";
+    }
+}
+
+class StudentTable
+{
+    private List<Student> students = new List<Student>();
+
+    public Student this[int index]
+    {
+        get { return students[index]; }
+        set { students[index] = value; }
+    }
+
+    public int StudentCount
+    {
+        get { return students.Count; }
+    }
+
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+    }
+    
+    public void UpdateStudent(int index, string firstName, string lastName, string middleName)
+    {
+        if (index >= 0 && index < students.Count)
+        {
+            students[index].FirstName = firstName;
+            students[index].LastName = lastName;
+            students[index].MiddleName = middleName;
+        }
+        else
+        {
+            Console.WriteLine("Помилка: неправильний індекс студента.");
+        }
+    }
+}
